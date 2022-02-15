a29a571f1b2c2448985302c36ff0e8d1a64aadc9
K-Nishikawa-taka <mamesuke19941213@gamil.com>
2022-02-15 15:20:14 +0000

    途中、絞り込み実装前

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,14 @@
 class SearchesController < ApplicationController
   
   def search
+    #検索ワードを取得
+    @range = params[:range]
+    
+    if @range == 'Genre'
+      @genres = Genre.looks(params[:word])
+    else
+      @rooms = Room.looks(params[:word])
+    end
   end
   
 end
