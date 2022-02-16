22b204ee7b2cb4e69ae3c2b883ef1af6da2a480e
K-Nishikawa-taka <mamesuke19941213@gamil.com>
2022-02-16 09:03:30 +0000

    検索機能

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,9 +5,9 @@ class SearchesController < ApplicationController
     @range = params[:range]
     
     if @range == 'Genre'
-      @genres = Genre.looks(params[:word])
+      @genres = Genre.looks(params[:key_word])
     else
-      @rooms = Room.looks(params[:word])
+      @rooms = Room.looks(params[:key_word])
     end
   end
   
