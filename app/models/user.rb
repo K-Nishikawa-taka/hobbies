100e06b186a47eb4dca421033438e3e9f3190e8e
K-Nishikawa-taka <mamesuke19941213@gmail.com>
2022-03-12 04:22:10 +0000

    エラー文作成中

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,17 +34,6 @@ class User < ApplicationRecord
     followings.include?(user)
   end
   
-  def create_notification_follow!(current_user)
-    temp = Notification.where(["visitor_id = ? and visited_id = ? and action = ? ",current_user.id, id, 'follow'])
-    if temp.blank?
-      notification = current_user.active_notifications.new(
-        visited_id: id,
-        action: 'follow'
-      )
-      notification.save if notification.valid?
-    end
-  end
-
   # is_deletedがfalseの時にtrueを返す
   def active_for_authentication?
     super && (is_deleted == false)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_09_102014) do
+ActiveRecord::Schema.define(version: 2022_03_12_035927) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
